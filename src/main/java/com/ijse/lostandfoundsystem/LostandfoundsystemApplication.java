38be8cfd510cf686286792f9f493c6f69ca4bdfc
Ravindu Gunawardana <ravindugunawardana7@gmail.com>
2025-05-05 21:48:01 +0530

    Initial commit

---
+++ src/main/java/com/ijse/lostandfoundsystem/LostandfoundsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.lostandfoundsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandfoundsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostandfoundsystemApplication.class, args);
+	}
+
+}
