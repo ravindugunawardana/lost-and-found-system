674ebd5849bfde281d379c258c33cf0d04ef1fc7
Ravindu Gunawardana <ravindugunawardana7@gmail.com>
2025-09-24 17:10:14 +0530

    Adding jwt dependencies, Security config & customer user details service

--- src/main/java/com/ijse/lostandfoundsystem/LostandfoundsystemApplication.java
+++ src/main/java/com/ijse/lostandfoundsystem/LostandfoundsystemApplication.java
@@ -1,10 +1,19 @@
 package com.ijse.lostandfoundsystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class LostandfoundsystemApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LostandfoundsystemApplication.class, args);
